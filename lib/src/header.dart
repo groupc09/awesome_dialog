7b5ab1f93411fa7b22bea322baa7aa186a30072a
mrwickk <vermavvv007@gmail.com>
2025-10-27 16:55:11 +0530

    rive is removed from this

--- lib/src/header.dart
+++ lib/src/header.dart
@@ -1,5 +1,4 @@
 import 'package:awesome_dialog/awesome_dialog.dart';
-import 'package:awesome_dialog/src/anims/rive_anim.dart';
 import 'package:flutter/material.dart';
 
 ///Header of the [AwesomeDialog]
@@ -21,36 +20,42 @@ class AwesomeDialogHeader extends StatelessWidget {
   Widget build(BuildContext context) {
     switch (dialogType) {
       case DialogType.info:
-        return RiveAssetAnimation(
+        return Icon(Icons.info, color: Colors.green);
+        /*return RiveAssetAnimation(
           assetPath: 'packages/awesome_dialog/assets/rive/info.riv',
           animName: loop ? 'appear_loop' : 'appear',
-        );
+        );*/
       case DialogType.infoReverse:
-        return RiveAssetAnimation(
-          assetPath: 'packages/awesome_dialog/assets/rive/info_reverse.riv',
-          animName: loop ? 'appear_loop' : 'appear',
-        );
+        return Icon(Icons.info, color: Colors.green);
+        // return RiveAssetAnimation(
+        //   assetPath: 'packages/awesome_dialog/assets/rive/info_reverse.riv',
+        //   animName: loop ? 'appear_loop' : 'appear',
+        // );
 
       case DialogType.question:
-        return RiveAssetAnimation(
-          assetPath: 'packages/awesome_dialog/assets/rive/question.riv',
-          animName: loop ? 'appear_loop' : 'appear',
-        );
+        return Icon(Icons.question_mark, color: Colors.blue,);
+        // return RiveAssetAnimation(
+        //   assetPath: 'packages/awesome_dialog/assets/rive/question.riv',
+        //   animName: loop ? 'appear_loop' : 'appear',
+        // );
       case DialogType.warning:
-        return RiveAssetAnimation(
-          assetPath: 'packages/awesome_dialog/assets/rive/warning.riv',
-          animName: loop ? 'appear_loop' : 'appear',
-        );
+        return Icon(Icons.warning, color: Colors.yellow,);
+        // return RiveAssetAnimation(
+        //   assetPath: 'packages/awesome_dialog/assets/rive/warning.riv',
+        //   animName: loop ? 'appear_loop' : 'appear',
+        // );
       case DialogType.error:
-        return RiveAssetAnimation(
-          assetPath: 'packages/awesome_dialog/assets/rive/error.riv',
-          animName: loop ? 'appear_loop' : 'appear',
-        );
+        return Icon(Icons.error, color: Colors.red,);
+        // return RiveAssetAnimation(
+        //   assetPath: 'packages/awesome_dialog/assets/rive/error.riv',
+        //   animName: loop ? 'appear_loop' : 'appear',
+        // );
       case DialogType.success:
-        return RiveAssetAnimation(
-          assetPath: 'packages/awesome_dialog/assets/rive/success.riv',
-          animName: loop ? 'appear_loop' : 'appear',
-        );
+        return Icon(Icons.check_box, color: Colors.green,);
+        // return RiveAssetAnimation(
+        //   assetPath: 'packages/awesome_dialog/assets/rive/success.riv',
+        //   animName: loop ? 'appear_loop' : 'appear',
+        // );
       case DialogType.noHeader:
         return const SizedBox.shrink();
     }
