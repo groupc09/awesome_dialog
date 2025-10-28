d6ca935798a074c37699514a368579fe889410db
mrwickk <vermavvv007@gmail.com>
2025-10-28 11:39:30 +0530

    modified hte Icons size

--- lib/src/header.dart
+++ lib/src/header.dart
@@ -20,38 +20,38 @@ class AwesomeDialogHeader extends StatelessWidget {
   Widget build(BuildContext context) {
     switch (dialogType) {
       case DialogType.info:
-        return Icon(Icons.info, color: Colors.green);
+        return Expanded(child: Icon(Icons.info, color: Colors.green));
         /*return RiveAssetAnimation(
           assetPath: 'packages/awesome_dialog/assets/rive/info.riv',
           animName: loop ? 'appear_loop' : 'appear',
         );*/
       case DialogType.infoReverse:
-        return Icon(Icons.info, color: Colors.green);
+        return Expanded(child: Icon(Icons.info, color: Colors.lightGreen));
         // return RiveAssetAnimation(
         //   assetPath: 'packages/awesome_dialog/assets/rive/info_reverse.riv',
         //   animName: loop ? 'appear_loop' : 'appear',
         // );
 
       case DialogType.question:
-        return Icon(Icons.question_mark, color: Colors.blue,);
+        return Expanded(child: Icon(Icons.question_mark, color: Colors.blue,));
         // return RiveAssetAnimation(
         //   assetPath: 'packages/awesome_dialog/assets/rive/question.riv',
         //   animName: loop ? 'appear_loop' : 'appear',
         // );
       case DialogType.warning:
-        return Icon(Icons.warning, color: Colors.yellow,);
+        return Expanded(child: Icon(Icons.warning, color: Colors.yellow,));
         // return RiveAssetAnimation(
         //   assetPath: 'packages/awesome_dialog/assets/rive/warning.riv',
         //   animName: loop ? 'appear_loop' : 'appear',
         // );
       case DialogType.error:
-        return Icon(Icons.error, color: Colors.red,);
+        return Expanded(child: Icon(Icons.error, color: Colors.red,));
         // return RiveAssetAnimation(
         //   assetPath: 'packages/awesome_dialog/assets/rive/error.riv',
         //   animName: loop ? 'appear_loop' : 'appear',
         // );
       case DialogType.success:
-        return Icon(Icons.check_box, color: Colors.green,);
+        return Expanded(child: Icon(Icons.check_box, color: Colors.green,));
         // return RiveAssetAnimation(
         //   assetPath: 'packages/awesome_dialog/assets/rive/success.riv',
         //   animName: loop ? 'appear_loop' : 'appear',
